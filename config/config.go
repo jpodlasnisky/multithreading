2d32b9ae6d15b91f1ac196e8217ad29141f307b9
João Pedro Podlasnisky dos Santos <jpodlasnisky@gmail.com>
2024-05-21 10:01:54 -0300

    Changing config and adding config.env

--- config/config.go
+++ config/config.go
@@ -14,7 +14,7 @@ func LoadConfig(path string) (Config, error) {
 	viper.SetConfigName("app_config")
 	viper.SetConfigType("env")
 	viper.AddConfigPath(path)
-	viper.SetConfigFile(".env")
+	viper.SetConfigFile("config.env")
 	viper.AutomaticEnv()
 	err := viper.ReadInConfig()
 	if err != nil {
